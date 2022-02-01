975fd56816466c65462ab1af90783c61589de6eb
Lucky Baar <git+me@leb4r.io>
2022-02-01 11:51:07 -0600

    scripts: add cli args to setup-minikube script

--- scripts/setup-minikube.sh
+++ scripts/setup-minikube.sh
@@ -2,18 +2,73 @@
 
 # see https://github.com/kubernetes/minikube/issues/7923#issuecomment-920018825
 
-KUBERNETES_VERSION="stable"
-MINIKUBE_ARGS="--feature-gates=LocalStorageCapacityIsolation=false"
+readonly KUBERNETES_VERSION="stable"
+readonly MINIKUBE_ARGS="--feature-gates=LocalStorageCapacityIsolation=false"
 
-if ! command -v minikube 2>/dev/null; then
-	echo "minikube is not installed..."
-	exit 1
-fi
+readonly -a MINIKUBE_ADDONS=(
+	"metrics-server"
+)
 
-minikube delete
-minikube start \
-	--kubernetes-version=$KUBERNETES_VERSION \
-	$MINIKUBE_ARGS
+function _minikube_check() {
+	command -v minikube 2>/dev/null
+}
+
+function _delete_minikube_cluster() {
+	minikube delete
+}
+
+# start the cluster with a given kubernetes version
+function _start_minikube_cluster() {
+	local version="${1:-"stable"}" args=$2
+
+	minikube start \
+		--kubernetes-version=$version \
+		$args
+}
 
 # make sure certain addons are available
-minikube addons enable metrics-server
+function _enable_minikube_addons() {
+	local addons=$@
+	minikube addons enable ${addons[*]}
+}
+
+## CLI handlers
+
+function handle_delete() {
+	_delete_minikube_cluster
+}
+
+function handle_start() {
+	_start_minikube_cluster $KUBERNETES_VERSION $MINIKUBE_ARGS
+	_enable_minikube_addons $MINIKUBE_ADDONS
+}
+
+function parse_args() {
+	while [[ $1 ]]; do
+		echo "Handling [$1]..."
+		case "$1" in
+		--all)
+			handle_delete
+			handle_start
+			shift
+			break
+			;;
+		--delete)
+			handle_delete
+			shift
+			break
+			;;
+		--start)
+			handle_start
+			shift
+			break
+			;;
+		*)
+			echo "Error: Unknown option: $1" >&2
+			exit 1
+			;;
+		esac
+	done
+}
+
+parse_args $@
